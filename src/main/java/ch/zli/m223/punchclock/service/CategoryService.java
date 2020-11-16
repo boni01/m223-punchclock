25441d53a639aa4073263b4415b14e2175fe1836
boni01 <bleron.rexhepi.ch@gmail.com>
2020-11-16 11:20:54 +0100

    Added Category

---
+++ src/main/java/ch/zli/m223/punchclock/service/CategoryService.java
@@ -0,0 +1,30 @@
+package ch.zli.m223.punchclock.service;
+
+import ch.zli.m223.punchclock.domain.Category;
+import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.repository.CategoryRepository;
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category createCategory(Category category){
+        return this.categoryRepository.saveAndFlush(category);
+    }
+
+    public List<Category> findAll() {
+        return this.categoryRepository.findAll();
+    }
+
+    public void deleteCategory(long id){
+        this.categoryRepository.deleteById(id);
+    }
+}
